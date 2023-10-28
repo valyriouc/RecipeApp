3a69f2b286aba62dc769ee7a3c374466a1ec6362
Valacor <valyrious.informatics@gmail.com>
2023-10-28 17:59:57 +0200

    Initial commit

---
+++ RecipeApi.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace RecipeApi.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ RecipeClient.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace RecipeClient.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
