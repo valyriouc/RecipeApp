4babfef377fe18b3337f16a5191c122ac8872599
Valacor <valyrious.informatics@gmail.com>
2023-11-03 21:08:36 +0100

    Creating user model class

---
+++ RecipeClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace RecipeClient
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application app = new Application();
+
+            MainWindow window = new MainWindow();
+
+            app.Run(window);
+        }
+    }
+}
